415e1261c23d14c8b6f0a92321b5b09fc44e2b62
Yaroslav Dronskii <jaroslav@dronsky.ru>
2016-03-05 23:32:53 +0300

    Fix for proximity wakeup
    The problem:
    Panel is being set to "busy" state after executing double tap gesture.
    If the phone is not woken up (i.e. PowerManager decided against it due to proximity sensor readings), panel is never reset to idle.
    And because of that, all subsequent doubletap events are ignored.
    
    The fix:
    atomic_set panel to idle the panel after sending doubletap uevent but before "pressing" the power button.
    This creates an unnecesary "free->busy->free->busy" transition, but it works.
    
    Change-Id: I046a992769669355df74360b6ffbf5e3ffada1a6

--- drivers/input/touchscreen/g3/lge_touch_core.c
+++ drivers/input/touchscreen/g3/lge_touch_core.c
@@ -136,6 +136,7 @@ void send_uevent_lpwg(struct i2c_client* client, int type)
 			&& atomic_read(&ts->state.uevent_state) == UEVENT_IDLE) {
 		atomic_set(&ts->state.uevent_state, UEVENT_BUSY);
 		send_uevent(lpwg_uevent[type-1]);
+                atomic_set(&ts->state.uevent_state, UEVENT_IDLE);
 		if (type == LPWG_DOUBLE_TAP) {
 			input_report_key(ts->input_dev, KEY_POWER, BUTTON_PRESSED);
 			input_report_key(ts->input_dev, KEY_POWER, BUTTON_RELEASED);
